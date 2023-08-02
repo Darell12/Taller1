6b172c876d735804a46fa3750202d6ffd8830d5d
darell12 <darellorlandoe@gmail.com>
2023-08-01 23:31:18 -0500

    Pruebas

--- app/src/main/java/com/example/taller1/Liquidacion.java
+++ app/src/main/java/com/example/taller1/Liquidacion.java
@@ -9,6 +9,8 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.text.DecimalFormat;
+
 public class Liquidacion extends AppCompatActivity {
 
     @Override
@@ -23,22 +25,33 @@ public class Liquidacion extends AppCompatActivity {
         TextView Txtcargo = findViewById(R.id.cargosLiq);
         TextView Txtdias = findViewById(R.id.diasLiq);
         TextView TxtdiaV = findViewById(R.id.VDiaLiq);
+        TextView Txtbase = findViewById(R.id.sueldoBaseLiq);
 
         String nombres = Datos.getStringExtra("nombres");
         String apellidos = Datos.getStringExtra("apellidos");
         String NombreCompleto = nombres + ' ' + apellidos;
+        double sueldoBase = Datos.getDoubleExtra("Base", 0);
+        double sueldoDia = Datos.getDoubleExtra("SueldoXdia", 0);
+        double sueldoNeto = Datos.getDoubleExtra("SueldoNeto", 0);
         TxtNombres.setText(NombreCompleto);
 
-        String cargo = Datos.getStringExtra("cargo");
+        String cargo = Datos.getStringExtra("Cargo");
         Txtcargo.setText(cargo);
 
         String dias = Datos.getStringExtra("Dias");
         Txtdias.setText(dias);
 
-        Integer sueldoDia = Datos.getIntExtra("SueldoXdia", 0);
-        TxtsueldoNeto.setText(String.valueOf(sueldoDia));
 
-        double sueldoNeto = Datos.getDoubleExtra("SueldoNeto", 0);
-        TxtsueldoNeto.setText(String.valueOf(sueldoNeto));
+        DecimalFormat formatoDosDecimales = new DecimalFormat("#.##");
+
+        // Formatear los valores sueldoBase, sueldoDia y sueldoNeto a dos decimales
+        String sueldoBaseFormateado = formatoDosDecimales.format(sueldoBase);
+        String sueldoDiaFormateado = formatoDosDecimales.format(sueldoDia);
+        String sueldoNetoFormateado = formatoDosDecimales.format(sueldoNeto);
+
+        // Actualizar las TextViews con los valores formateados
+        Txtbase.setText(sueldoBaseFormateado);
+        TxtdiaV.setText(sueldoDiaFormateado);
+        TxtsueldoNeto.setText(sueldoNetoFormateado);
     }
 }
